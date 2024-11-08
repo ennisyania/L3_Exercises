b20b481ff2e674067afe50f39511c873bec0d9e7
ennisyania <ennisyania@gmail.com>
2024-11-08 15:39:32 +0800

    Lesson 3

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {Button, Alert, StyleSheet, Text, TextInput, View, TouchableOpacity, ToastAndroid} from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+const InputBox =({label, onChangeText})=>{
+    return(
+        <View>
+            <Text>
+                {label}
+            </Text>
+            <TextInput
+                style={{borderWidth: 1}} onChangeText={onChangeText}
+                />
+        </View>
+    );
+};
+// custom component to quickly make input boxes
+
+const MyApp = () => {
+    const [pw, setPw] = useState('');
+    const [name, setName] = useState('');
+    const [type, setType] = useState('');
+
+
+
+    return (
+      <View style={{ padding: 20, paddingTop: 50}}>
+          <Text>User Type:</Text>
+          <RNPickerSelect
+              onValueChange={(value) => setType(value)}
+              items={[
+                  {label: 'Admin', value: 'Admin'},
+                  {label: 'Guest', value: 'Guest'},
+              ]}
+          />
+          <InputBox label={"User Name:"} onChangeText={(text) => setName(text)} />
+          <InputBox label={"Password:"} onChangeText={(text) => setPw(text)} />
+
+
+
+          {/*<Button*/}
+          {/*    title={"LOG IN"}*/}
+          {/*    onPress={()=>{Alert.alert("Another Thing!")}}*/}
+          {/*/>*/}
+          <TouchableOpacity
+              onPress={() => {
+                  ToastAndroid.show("Welcome " + type+" " + name, ToastAndroid.SHORT);
+              }}>
+              <Text>LOG IN</Text>
+          </TouchableOpacity>
+      </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default MyApp;
+
+
